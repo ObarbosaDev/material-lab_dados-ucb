a3e02b9baf8d2fdb2cef2b20c1c8a0429cd0daaa
ObarbosDev <opinheirodev@gmail.com>
2026-03-18 23:55:10 -0300

    Atividade 01

--- atividade1/criar_tabela_livro.sql
+++ atividade1/criar_tabela_livro.sql
@@ -1,10 +1,9 @@
-CREATE TABLE livro(
+CREATE TABLE livro (
     id_livro INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
     sinopse TEXT NOT NULL,
     autor VARCHAR(128) NOT NULL,
     editora VARCHAR(128) NOT NULL,
     categoria VARCHAR(128) NOT NULL,
-    ano_publicacao YEAR NOT NULL,
-    estoque INT NOT NULL DEFAULT 0 CHECK (estoque >= 0) -- Nova coluna
+    ano_publicacao YEAR NOT NULL
 );
